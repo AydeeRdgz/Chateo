661bad7150a6ee7b2fcbb6bcb8a91b81709ada61
AydeeRdgz <aydeerodriguezg@gmail.com>
2021-07-24 20:59:45 -0400

    Set up husky and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,32 +3,30 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-typescript',
-  ],
+  extends: ["airbnb-typescript", "prettier", "plugin:prettier/recommended"],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
       parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
       },
-    }
+    },
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
-    project: './tsconfig.json'
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
   },
 };
